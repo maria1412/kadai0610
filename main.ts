ba0d02fcb2e91b07956c5bfc744896f458b06a65
maria1412 <82562294+maria1412@users.noreply.github.com>
2021-06-14 12:11:28 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,20 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 4; index++) {
-    	
-    }
+    sprite.change(LedSpriteProperty.Y, -1)
 })
-let スプライト = 0
+let sprite: game.LedSprite = null
+sprite = game.createSprite(2, 4)
 basic.forever(function () {
-	
+    if (input.acceleration(Dimension.X) < -150) {
+        sprite.change(LedSpriteProperty.X, -1)
+        basic.pause(100)
+    } else if (input.acceleration(Dimension.X) > 150) {
+        sprite.change(LedSpriteProperty.X, 1)
+        basic.pause(100)
+    } else if (input.acceleration(Dimension.Y) < -150) {
+        sprite.change(LedSpriteProperty.Y, -1)
+        basic.pause(100)
+    } else if (input.acceleration(Dimension.Y) > 150) {
+        sprite.change(LedSpriteProperty.Y, 1)
+        basic.pause(100)
+    }
 })
